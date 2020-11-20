cc591e8add16864f4e7a4e3459b6ae91f2b5e169
JvPelai <jv.pelai@gmail.com>
2020-11-20 19:48:45 -0300

    DAO + sqlite3

--- casadocodigo/src/app/routes/routes.js
+++ casadocodigo/src/app/routes/routes.js
@@ -1,3 +1,6 @@
+const LivroDao = require('../infra/livro-dao');
+const db = require('../../config/database');
+
 module.exports = (app) => {
     app.get('/', function(req,resp){
         resp.send(
@@ -16,18 +19,16 @@ module.exports = (app) => {
     });
     
     app.get('/livros',function(req,resp){
-        resp.send(
-            `
-            <html>
-            <head>
-            <meta charset="utf-8">
-            </head>
-            <body>
-            <h1> Listagem de livros </h1>
-            </body>
-            </html>
-            `
-        ); 
+        const livroDao = new LivroDao(db);
+        livroDao.lista()
+        .then(livros => resp.marko(
+            require('../views/livros/lista/lista.marko'),
+                {
+                    livros: livros
+                }
+        ))
+        .catch(erro => console.log(erro));
+        
     });
 
 };
