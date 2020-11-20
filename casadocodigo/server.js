96d80c6c4a6b5e60e6d9cd3f95dacc9572eeb258
JvPelai <jv.pelai@gmail.com>
2020-11-20 17:26:04 -0300

    modularização e bibliotecas

--- casadocodigo/server.js
+++ casadocodigo/server.js
@@ -1,39 +1,12 @@
-const express = require('express');
-const app = express();
-
-app.get('/', function(req,resp){
-    resp.send(
-
-        `
-        <html>
-        <head>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1> Casa do Código </h1>
-        </body>
-        </html>
-        `
-    );
-});
-app.get('/livros',function(req,resp){
-    resp.send(
-        `
-        <html>
-        <head>
-        <meta charset="utf-8">
-        </head>
-        <body>
-        <h1> Listagem de livros </h1>
-        </body>
-        </html>
-        `
-    ); 
-});
-    
+const app = require ('./src/config/custom-express.js');
 
 
 app.listen(3000, function(){
     console.log('listening on port 3000');
 });
+
+
+    
+
+
 
