339f4a503449e8a9fede603d01016e431b792918
JvPelai <jv.pelai@gmail.com>
2020-11-23 18:17:30 -0300

    express initial setup

--- REST/projetoapi/index.js
+++ REST/projetoapi/index.js
@@ -3,3 +3,7 @@ const express = require('express')
 const app = express()
 
 app.listen(3000, () => console.log('listening on port 3000'))
+
+app.get('/atendimentos' ,(req,res) => {
+    res.send('Servidor no ar! Você está na area de atendimentos');
+})
