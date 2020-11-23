6aefd2718c7b038a065e3c123f50a049f9b17a2f
JvPelai <jv.pelai@gmail.com>
2020-11-23 12:52:44 -0300

    streaming de dados

---
+++ REST/payfast/controllers/uploads.js
@@ -0,0 +1,15 @@
+var fs = require('fs');
+
+module.exports = function(app){
+    app.post("/upload/imagem", function(req,res){
+        console.log('recebendo imagem');
+
+        var filename = req.headers.filename;
+
+        req.pipe(fs.createWriteStream('files/' + filename))
+            .on('finish', function(){
+                console.log('arquivo salvo');
+                res.status(201).send('ok');
+            });
+    });
+}
